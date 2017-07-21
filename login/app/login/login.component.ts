85a5139ed7f50dc91ace5d90de49694c7dadc888
Alexander Djenkov <djenkov@progress.com>
2017-07-21 15:24:40 +0300

    Make Login page SASS ready

--- login/app/login/login.component.ts
+++ login/app/login/login.component.ts
@@ -3,27 +3,47 @@ import { Component, OnInit } from "@angular/core";
 @Component({
     selector: "Login",
     moduleId: module.id,
-    templateUrl: "./login.component.html",
+    templateUrl: "./login.component.html"
 })
 export class LoginComponent implements OnInit {
     email: string;
     password: string;
 
     constructor() {
+        /* ***********************************************************
+        * Use the constructor to inject app services that will be needed for
+        * the whole login layout as a whole.
+        *************************************************************/
     }
 
     ngOnInit(): void {
+        /* ***********************************************************
+        * Use the "ngOnInit" handler to initialize data for the whole
+        * login layout as a whole.
+        *************************************************************/
     }
 
-    onLoginFacebookButtonTap() {
+    /* ***********************************************************
+    * TODO: Implement Facebook login.
+    *************************************************************/
+    onLoginFacebookButtonTap(): void {
     }
 
-    onLoginGoogleButtonTap() {
+    /* ***********************************************************
+    * TODO: Implement Google login.
+    *************************************************************/
+    onLoginGoogleButtonTap(): void {
     }
 
-    onSignInButtonTap() {
+    /* ***********************************************************
+    * TODO: Implement custom sign in.
+    *************************************************************/
+    onSignInButtonTap(): void {
     }
 
-    onForgotPasswordTap() {
+    /* ***********************************************************
+    * TODO: Implement forgot password.
+    *************************************************************/
+    onForgotPasswordTap(): void {
     }
 }
--- login/app/login/login.module.ts
+++ login/app/login/login.module.ts
@@ -1,8 +1,8 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
 import { NativeScriptModule } from "nativescript-angular/nativescript.module";
 
-import { LoginComponent } from "./login.component";
 import { LoginRoutingModule } from "./login-routing.module";
+import { LoginComponent } from "./login.component";
 
 @NgModule({
     imports: [
